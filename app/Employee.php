c87a7740927fe4ac76e7cbd48572d5e4d1b3ca26
juniarsofyan <choerulsofyanmf@gmail.com>
2020-07-15 11:56:46 +0700

    Employee relationship with Position & Attendance

--- app/Employee.php
+++ app/Employee.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     protected $fillable = [ 'nik', 'name', 'birthdate', 'gender', 'phone', 'email', 'position_id', 'user_id', 'address'];
+
+    public function position()
+    {
+    	return $this->belongsTo('App\Position');
+    }
+
+    public function attendances()
+    {
+    	return $this->hasMany('App\Attendance');
+    }
 }
